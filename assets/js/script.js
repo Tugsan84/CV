574564d602cddaf3b9a7370aa7bc07c68953fb11
Tugsan84 <nttorunoglu1984@hotmail.com>
2025-11-23 07:31:45 +0300

    hamburger menü düzeltildi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 
-hamburger.addEventListener('click', () => {
+// Hamburger týklanýnca menüyü aç/kapa
+hamburger.addEventListener('click', (e) => {
     nav.classList.toggle('active');
+    e.stopPropagation(); // click eventinin document'e yayýlmasýný engeller
+});
+
+// Nav menüye týklanýnca menü kapanmasýn
+nav.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
+
+// Sayfaya týklanýnca menüyü kapat
+document.addEventListener('click', () => {
+    nav.classList.remove('active');
 });
