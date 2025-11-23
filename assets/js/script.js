3487fc781629c2d65d963abc6b5d832c65edd749
Tugsan84 <nttorunoglu1984@hotmail.com>
2025-11-23 07:24:19 +0300

    son hali responsive. ama eksikler var,sonra bak yine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,6 @@
-const header = document.querySelector("header");
-const placeholder = document.getElementById("header-placeholder");
-let initialOffset = header.offsetTop;
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
 
-function updateHeader() {
-    // scrollY header'ýn baþladýðý noktayý geçtiyse
-    if (window.scrollY > initialOffset) {
-        header.classList.add("fixed");
-        placeholder.style.height = header.offsetHeight + "px";
-    } else {
-        header.classList.remove("fixed");
-        placeholder.style.height = "0px";
-    }
-}
-
-// scroll ve resize event
-window.addEventListener("scroll", updateHeader);
-window.addEventListener("resize", () => {
-    // resize sonrasý header baþlangýcýný yeniden hesapla
-    initialOffset = header.offsetTop;
-    updateHeader();
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
 });
